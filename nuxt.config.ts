4f8f78032bcc7deaba9e7380a6505ac9a6451501
rvsalih <salihgngormez@icloud.com>
2024-05-03 12:32:14 +0300

    2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+      
+    },
+  ssr: {
+    noExternal: [ /\.css$/, /^vuetify/ ],
+  },
+  },
+  
 })
